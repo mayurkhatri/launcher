947c69447c37b98dd3128dcd94c5359e54ec5f4f
mayurkhatri <mayurkhatri122@gmail.com>
2017-11-05 16:50:34 +0530

    Add custom middleware

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative "../lib/my_middleware"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +13,6 @@ module Launcher
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.use "MyMiddleware"
   end
 end
